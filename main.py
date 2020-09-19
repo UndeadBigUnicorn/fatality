5fca5d8d2d8b4a0e47b155b96f56501ba039feaa
Serhii Fedusov <serhii.fedusov@depositphotos.com>
2020-09-19 22:27:12 +0300

    changed delimiter

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ if __name__ == '__main__':
     with open('template-responses.txt', 'r') as templates_responses:
         templates = []
         for template in templates_responses.readlines():
-            templates.append(tuple(template.split(':')))
+            templates.append(tuple(template.split('|')))
