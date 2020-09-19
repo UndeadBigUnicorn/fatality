d8f181740aa78525de42415ef3c8dcaaba9c654e
Serhii Fedusov <serhii.fedusov@depositphotos.com>
2020-09-19 22:33:21 +0300

    connected template-engine

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from template_engine import TemplateEngine
+
 if __name__ == '__main__':
     with open('template-responses.txt', 'r') as templates_responses:
         templates = []
         for template in templates_responses.readlines():
             templates.append(tuple(template.split('|')))
+        engine = TemplateEngine(templates)
+        while True:
+            message = input()
+            print(engine.process_question(message))
